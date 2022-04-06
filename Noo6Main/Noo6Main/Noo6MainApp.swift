c0bf6f67eb0bbb956e7a77b38f71496515b048d0
YunSeok-Choi <dbstjr9898@naver.com>
2022-04-06 16:54:03 +0900

    Main

---
+++ Noo6Main/Noo6Main/Noo6MainApp.swift
@@ -0,0 +1,17 @@
+//
+//  Noo6MainApp.swift
+//  Noo6Main
+//
+//  Created by 최윤석 on 2022/04/06.
+//
+
+import SwiftUI
+
+@main
+struct Noo6MainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
