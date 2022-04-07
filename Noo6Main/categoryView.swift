eaf169868dc1ed1b615f64db9e1acdd163291a0a
bee712 <48711413+bee712@users.noreply.github.com>
2022-04-08 01:22:56 +0900

    가이드 이동 버튼 구조/생성방법 수정
    받은 Array 정보에 따라 가이드로 이동하기 위한 Button을 List로 생성

--- Noo6Main/categoryView.swift
+++ Noo6Main/categoryView.swift
@@ -9,75 +9,82 @@ import Foundation
 import SwiftUI
 
 struct categoryView : View {
-    var titleList : [String] = ["첫번째 제목", "두번째 제목", "세번째 제목", "네번째 제목"]
+
     var body: some View{
         VStack {
-            
-            HStack {
-                NavigationView{
-                    VStack{
-                        Text("아이폰 초보자 가이드에서 \n어떤 가이드를 받아 볼까요?")
-                            .frame(width: 342)
-                            .font(.system(size: 30))
-                            .padding(.bottom, 40)
-                            .multilineTextAlignment(.center)
-          
-                        //NavigationLink
-                        ForEach(titleList.identified(by: \.id)){
-                            
-                        }
-                        ContentButton(title: "잠금화면 제스처", checkValue : true)
-                        List{
-                            Text("1")
-                        }
-
-
-                        
-                        Spacer()
+            NavigationView{
+                VStack{
+                    Text("아이폰 초보자 가이드에서 \n어떤 가이드를 받아 볼까요?")
+                        .frame(width: 342)
+                        .font(.system(size: 30))
+                        .padding(.bottom, 40)
+                        .multilineTextAlignment(.center)
+                    
+                    List(contentInfos, id: \.id) {
+                        info in
+                        ContentList(contentInfo: info)
+                            .listRowInsets(EdgeInsets(top: 10, leading: 0, bottom: 10, trailing: 0))
+                            .listRowBackground(Color.white)
+                            .listRowSeparator(.hidden)
+                            .padding(.leading, 3)
                     }
+                    .listStyle(.plain)
+                    .background(Color.white)
+                    .edgesIgnoringSafeArea(.all)
+                    .padding()
                     
-                    
+                    Spacer() // 화면 상단에 List를 보여주기 위해 공백 삽입
                 }
-
+                
             }
-            
-            
             
         }.padding([.leading, .trailing], 16)
     }
     
-    
 }
 
+// List에 들어갈 Array의 구조
+struct ContentInfo: Identifiable {
+    let id = UUID()
+    let title: String
+    let isComplete: Bool
 
-struct ContentButton : View{
-    var title : String = ""
-    var checkValue : Bool
+}
 
+// List에 들어갈 Array에 데이터 넣기
+let contentInfos = [
+    ContentInfo(title: "잠금화면 제스처", isComplete: false),
+    ContentInfo(title: "앱 페이지의 기본 제스처", isComplete: true),
+    ContentInfo(title: "홈 화면의 기본 제스처", isComplete: false),
+    ContentInfo(title: "홈 화면을 편집하는 방법", isComplete: false)
+]
+
+// List에 들어갈 카테고리별 컨텐츠 목록 View
+struct ContentList : View {
+    var contentInfo : ContentInfo
     var body: some View{
-        Button(action : {}){
+        Button(action: {}){
             HStack{
-                Text("\(title)")
+                Text("\(contentInfo.title)")
                     .font(.system(size: 24))
                     .foregroundColor(.black)
                     .padding(.leading, 20)
                     //.background(Color.white).cornerRadius(15)
                 Spacer()
-                if checkValue{
+                if contentInfo.isComplete{
                     Image(systemName: "checkmark.circle")
                         .resizable()
                         .frame(width: 30, height: 30)
+                        .foregroundColor(.blue)
                         .padding()
                 }
             }
-            
-        }.frame(width: 342, height: 78)
+        }.frame(width: 320, height: 78)
             .cornerRadius(10).background(Color.white).cornerRadius(10).shadow(color: Color.gray, radius: 3, x: 0, y: 0)
-
-
         
     }
 }
+
 
 struct categoryView_Previews: PreviewProvider {
     static var previews: some View {
