bf0052529b271b0011e38739917230c48f65413f
bee712 <48711413+bee712@users.noreply.github.com>
2022-04-10 17:22:30 +0900

    ProgressTestView 완료
    다음단계 누르면 단계 Text와 ProgressView 변화, 모든 단계완료 후 다음단계 누를 시 ClearView()로 이동, 이전 단계 누르면 이전단계로 Text와 ProgressView 변화

--- Noo6Main/ProgressTestView.swift
+++ Noo6Main/ProgressTestView.swift
@@ -10,17 +10,46 @@ import SwiftUI
 
 
 struct ProgressTestView: View {
-    @State var progressUp : Float = 0
+    @State var progressUp : Double = 1/guideStageInfos.totalStage;
+    @State var isGuideComplete : Bool = false
+    @State var currentStage : Double = 1
+    @State var totalStage : Double = guideStageInfos.totalStage
+    
     var body: some View {
-        VStack{
-            ProgressView(value: progressUp)
-            NavigationView{
-                Button("Next Stage"){
-                    progressUp += 1/guideStageInfos.totalStage
-                    if (progressUp>=1){
-                        // 1 이상일 때만 ClearView로 이동하도록 코딩하기
+        NavigationView{
+            VStack{
+                
+                ProgressView(value: progressUp)
+                HStack{
+                    // 이전단계로 가는 Button
+                    Button("이전 단계"){ // 1단계 이전으로 돌아갈 수 없도록 조건 부여
+//                        if((progressUp - (1/guideStageInfos.totalStage)) > 1/guideStageInfos.totalStage){
+                        if(currentStage > 1){
+                            progressUp -= 1/guideStageInfos.totalStage
+                            currentStage -= 1
+                        }
+                    }
+                    Spacer()
+                    Text("\(Int(currentStage))")
+                    Text("/")
+                    Text("\(Int(guideStageInfos.totalStage))")
+                    Spacer()
+                    // 다음단계 및 ClearView로 가는 Button
+                    NavigationLink(destination: ClearView(), isActive: $isGuideComplete){
+                        // 현재 가이드의 끝일 때에만 ClearView로 이동하도록 isActive
+                        Button("다음 단계"){
+                            if(progressUp >= 0.98){
+                                isGuideComplete = true
+                            }
+                            else{
+                                progressUp += 1/totalStage
+                                currentStage += 1
+                            }
+                        }
                     }
                 }
+                
+                
             }
             
         }
@@ -31,8 +60,9 @@ struct ProgressTestView: View {
 // CategoryView에서 받아올 Guide정보 구조 (테스트용)
 struct GuideStageInfo: Identifiable {
     let id = UUID()
-    let title: String
-    let totalStage: Float
+    let title: String   // 현재 가이드 타이틀 이름
+    let totalStage: Double // 현재 가이드 총 스테이지
+    // 현재 음소거/소리 모드 상태 변수 : Bool-> 이 경우 데이터베이스에 정보 업데이트 필요
 
 }
  
