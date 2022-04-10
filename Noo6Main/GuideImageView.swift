05accb38306f23a19d021b3061bc378eca83c62f
bee712 <48711413+bee712@users.noreply.github.com>
2022-04-10 15:17:36 +0900

    GuideImageView 구조체 형식 수정
    융식이 병합한 메인 브런치의 내용을 가져와서 병합시킨 후 GuidImageView 구조체 일부 수정

--- Noo6Main/GuideImageView.swift
+++ Noo6Main/GuideImageView.swift
@@ -34,14 +34,16 @@ struct GuideImageView: View {
 }
 struct GuideList: Identifiable, Hashable{//가이드리스트 구조체, 이미지와 설명넣기
     let id = UUID() //UUID = 고유식별자
+    let title : String // 현재 가이드의 제목
     let image: String
+    let totalStage : Float // 현재 가이드의 총 단계
     let explain: String
 }
 
 let guidelists = [
-    GuideList(image: "image1",explain: "설명란입니다1." ),
-    GuideList(image: "image2",explain: "설명란입니다2." ),
-    GuideList(image: "image3",explain: "설명란입니다3." )
+    GuideList(title: "잠금화면", image: "image1", totalStage : 7, explain: "설명란입니다1." ),
+    GuideList(title : "test2", image: "image2", totalStage : 5, explain: "설명란입니다2." ),
+    GuideList(title : "test2", image: "image3", totalStage : 4, explain: "설명란입니다3." )
     //똑같이 리스트로 추가
 ]
 struct ImageView_Previews: PreviewProvider {
