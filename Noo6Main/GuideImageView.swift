3f0841ec9f387d756e24dfe34bb1ba083f373159
hurdasol98 <73656470+hurdasol98@users.noreply.github.com>
2022-04-10 11:11:54 +0900

    GuideImageView 버튼 작동
    type mismatch 해결
    struct의 id를 UUID()  -->  Int

--- Noo6Main/GuideImageView.swift
+++ Noo6Main/GuideImageView.swift
@@ -8,43 +8,46 @@
 import SwiftUI
 
 struct GuideImageView: View {
-    @State var currentPage: Int = 0
+    @State private var currentPage: Int = 0
     var body: some View {
-        VStack {
-                    TabView(selection: $currentPage) {
-                        ForEach(0..<5) { i in
-                            ZStack {
-                                [Color.yellow, .red, .blue].randomElement()
-                                Text("\(i)")
-                            }.clipShape(Rectangle())
-                        }
-                    }.tabViewStyle(PageTabViewStyle())
-
-                    Button("Next") {
-                        if currentPage == 4 {
-                            currentPage = 0
-                            //return
-                        }else{
-                            currentPage += 1
-                        }
-
+        
+        VStack{
+            TabView(selection: $currentPage){
+                ForEach(guidelists){i in
+                    VStack{
+                        Text(i.explain)
+                        Image(i.image)
+                            .resizable()
                     }
+                }
+            }.tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))  //page처럼 구현 + ...을 안보이게함
+            
+            Button("Next") {
+                if currentPage == guidelists.count {
+                    currentPage = 0
+                    //return
+                }else{
+                    currentPage += 1
+                }
+            }
         }
     }
 }
+
 struct GuideList: Identifiable, Hashable{//가이드리스트 구조체, 이미지와 설명넣기
-    let id = UUID() //UUID = 고유식별자
+    let id: Int
     let image: String
     let explain: String
 }
 
 let guidelists = [
-    GuideList(image: "image1",explain: "설명란입니다1." ),
-    GuideList(image: "image2",explain: "설명란입니다2." ),
-    GuideList(image: "image3",explain: "설명란입니다3." )
+    GuideList(id: 0, image: "image1",explain: "explain1." ),
+    GuideList(id: 1, image: "image2",explain: "explain2." ),
+    GuideList(id: 2, image: "image3",explain: "explain3." )
     //똑같이 리스트로 추가
 ]
-struct ImageView_Previews: PreviewProvider {
+
+struct ImageView_Previews: PreviewProvider   {
     static var previews: some View {
         GuideImageView()
     }
