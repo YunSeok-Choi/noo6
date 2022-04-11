eda08e7858744af48be053c9f39d51912a96eaf5
bee712 <48711413+bee712@users.noreply.github.com>
2022-04-11 22:50:12 +0900

    Json 파일 추가
    CategoryData : CategoryView와 HomeView에서 사용할 데이터
    ClearData : ClearView HonorView에서 사용할 이미지, 텍스트 데이터
    DataStruct : Json파일 구조체 정의

--- Noo6Main/DataStruct.swift
+++ Noo6Main/DataStruct.swift
@@ -8,7 +8,37 @@
 import Foundation
 
 struct HomeData : Codable, Identifiable {
-    var id : Int
+    let id : Int
     let title : String
     let isAllCleared : Bool
+}
+// HomeView와 CategoryView에서 사용할 데이터
+struct CategoryStorage : Codable, Identifiable {
+    let id : Int
+    let categoryTitle : String
+    let categoryIcon : String
+    let isAllCleared : Bool
+    let guideInfo : Array<CategoryInfo>
+    let completedNumber : Int
+    let allNumber : Int
+}
+
+struct CategoryInfo : Codable, Identifiable {
+    let id : Int
+    let guideName : String
+    let isComplete : Bool
+}
+
+
+
+// ClearView에서 사용할 데이터
+struct ClearStorage : Codable, Identifiable {
+    let id : Int
+    let clearInfo : Array<ClearInfo>
+}
+
+struct ClearInfo : Codable, Identifiable {
+    let id : Int
+    let clearImage : String
+    let clearMessage : String
 }
